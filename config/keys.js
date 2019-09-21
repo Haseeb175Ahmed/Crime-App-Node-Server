721fc749e06af2d808567945d0a6fede8b342f97
DivorDriver <divordriver@gmail.com>
2019-09-21 13:27:33 +0100

    Login/Register Done Apis

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: "mongodb+srv://crimeApp:masroor_13@cluster0-xqymo.mongodb.net/test?retryWrites=true&w=majority" 
+    // mongoURI: "mongodb://kashif:kashif36@ds117909.mlab.com:17909/expertizodatabase" 
+  };
+
+  module.exports = {
+    mongoURI: "mongodb+srv://crimeApp:masroor_13@cluster0-xqymo.mongodb.net/dbCrimeApp?retryWrites=true&w=majority" ,
+    // mongoURI: "mongodb://crimeApp:masroor_13@ds117909.mlab.com:17909/expertizodatabase" ,
+    secretOrKey: "secret"
+  };
